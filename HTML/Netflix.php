9a0c49c86859db3ec25a668cdfaf480ea24894c1
pizzaimiplace <catalind333@gmail.com>
2024-06-23 05:39:46 +0300

    pagina show ului

---
+++ HTML/Netflix.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>MoX</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="netflix.css">
+    </head>
+    <body>
+        <header>
+            <img class="logo" src="../Images/logo.png" alt="MoX Logo">
+            <p>Netflix Shows</p>
+            <div class="pages-buttons-header">
+                <ul class="button-list">
+                    <li class="page-button"><a href="Netflix.php">Netflix</a></li>
+                    <li class="page-button"><a href="Disney.php">Disney</a></li>
+                    <li class="page-button"><a href="About.php">About</a></li>
+                    <li class="page-button"><a href="Help.php">Help</a></li>
+                </ul>
+            </div>
+        </header>
+        <div class="wrapper">
+            <div class="background-wrapper-netflix">
+                <img src="../Images/netflix.jpg" alt="Netflix">
+            </div>
+            <div class="pages-buttons">
+                <ul class="button-list">
+                    <li class="page-button"><a href="Netflix.php">Netflix</a></li>
+                    <li class="page-button"><a href="Disney.php">Disney</a></li>
+                    <li class="page-button"><a href="About.php">About</a></li>
+                    <li class="page-button"><a href="Help.php">Help</a></li>
+                </ul>
+            </div>
+            <div class="search-wrapper">
+                <div class="search-filters">
+                    <img src="../Images/3lines.png" alt="Filters">
+                </div>
+                <div class="search-container">
+                    <input type="text" placeholder="Search..." class="search-input">
+                    <button class="search-button">Go</button>
+                </div>
+            </div>
+            <div class="netflix-shows">
+                <div class="show-card">
+                    <img src="../Images/bcs_poster.jpg" alt="BCS">
+                </div>
+                <div class="show-card">
+                    <img src="../Images/got_poster.jpg" alt="GOT">
+                </div>
+                <div class="show-card">
+                    <img src="../Images/stranger_things.jpeg" alt="ST">
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
